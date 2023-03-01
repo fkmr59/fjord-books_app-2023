baab9e04cf31dada00322e0a36c0afd8d5f94843
terumitt <f4k0m6r0@icloud.com>
2023-03-01 13:58:14 +0900

    usermodelに入力値の制限を追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :profile, length: { maximum: 200 }
 end
