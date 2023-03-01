63613f645f5497bbdb07f2e49c1d8363ef00e0b5
terumitt <f4k0m6r0@icloud.com>
2023-03-01 16:08:45 +0900

    app/controllers/users_controller.rbの追記 app/views/users/index.html.erbの作成・追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def show
-    #def show
-      @user = User.find(params[:id])
-    #end
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
