1c9887c6b2e71790b3b103de7c88a372051a801c
terumitt <f4k0m6r0@icloud.com>
2023-03-02 10:48:31 +0900

    app/controllers/application_controller.rbにlogout後にログインページへの遷移パスを追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,9 @@ class ApplicationController < ActionController::Base
       keys: %i[email password postal_code address profile]
     )
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
 end
