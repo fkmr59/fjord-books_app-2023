283a5f159912e48b209503a4eb0016e4f1d51167
terumitt <f4k0m6r0@icloud.com>
2023-03-02 11:13:18 +0900

    app/controllers/application_controller.rbにログイン後の画面遷移とログアウト後の画面遷移を追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
     )
   end
 
+  def after_sign_in_path_for(_resource)
+    books_path
+  end
+
   def after_sign_out_path_for(_resource)
     new_user_session_path
   end
