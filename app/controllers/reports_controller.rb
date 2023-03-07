1a5ec90e86d6ccd2afab2c9b32ff35ff0a342b19
terumitt <f4k0m6r0@icloud.com>
2023-03-07 14:05:00 +0900

    app/controllers/reports_controller.rbの記述を変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,15 +21,16 @@ class ReportsController < ApplicationController
 
   # POST /reports or /reports.json
   def create
-    # @report = Report.new(report_params)
     @report = current_user.reports.build(report_params)
 
     respond_to do |format|
       if @report.save
-        format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
+        # format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
+        format.html { redirect_to report_url(@report) }
         format.json { render :show, status: :created, location: @report }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        # format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
