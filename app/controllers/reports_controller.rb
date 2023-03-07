4c6032079feb98aafe4f3a7db4e2b66a58edff0d
terumitt <f4k0m6r0@icloud.com>
2023-03-07 12:26:22 +0900

    [app/controllers/reports_controller.rb]に@report = current_user.reports.build(report_params)を変更追記

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,8 +21,9 @@ class ReportsController < ApplicationController
 
   # POST /reports or /reports.json
   def create
-    @report = Report.new(report_params)
-
+    # @report = Report.new(report_params)
+    @report = current_user.reports.build(report_params) 
+    
     respond_to do |format|
       if @report.save
         format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
