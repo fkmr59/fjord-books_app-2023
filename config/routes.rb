0be964f2ff9294627c61ae58110faff111fb8476
terumitt <f4k0m6r0@icloud.com>
2023-03-08 15:22:51 +0900

    config/routes.rbのコメントアウトしていた記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :users, only: %i(index show)
-  # resources :books
   resources :books do
-    # resources :comments, only: :create, module: :books
     resources :comments, only: %i[create destroy edit update], module: :books
   end
   # resources :reports
   resources :reports do
-    # resources :comments, only: :create, module: :reports
     resources :comments, only: %i[create destroy edit update], module: :reports
   end
   
