d34d73b455303b21b1dc374049cca2f8a85f494f
terumitt <f4k0m6r0@icloud.com>
2023-03-08 12:49:52 +0900

    app/controllers/comments_controller.rbにcommentのcreateを記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      # コメント作成失敗時の処理は後述
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
