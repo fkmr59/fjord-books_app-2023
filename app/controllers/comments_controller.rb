0f93ca5cbc82ed96b40f743a830579a8652c91d2
terumitt <f4k0m6r0@icloud.com>
2023-03-08 14:50:00 +0900

    app/controllers/comments_controller.rbに編集更新削除のコントローラーを追記

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@
 
 class CommentsController < ApplicationController
 
+  def edit; end
+
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user = current_user
@@ -11,6 +13,19 @@ class CommentsController < ApplicationController
       @comments = @commentable.comments
       render_commentable_show
     end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
