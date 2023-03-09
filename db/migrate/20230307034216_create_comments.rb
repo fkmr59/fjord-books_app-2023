8a7e771a83fb7653ae1b8f23c02f183ab30382fc
terumitt <f4k0m6r0@icloud.com>
2023-03-09 19:33:19 +0900

    db/migrate/20230307034216_create_comments.rbにnull: falseを追記

--- db/migrate/20230307034216_create_comments.rb
+++ db/migrate/20230307034216_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text  :content
-      t.references :imageable, polymorphic: true
+      t.references :imageable, polymorphic: true, null: false
       t.belongs_to :user, foreign_key: true
 
       t.timestamps
