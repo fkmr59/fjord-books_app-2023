45b46d0d064140cc420832b4697ced8570741c9c
terumitt <f4k0m6r0@icloud.com>
2023-03-14 17:30:39 +0900

    db/migrate/20230314080404_add_index_to_report_reference_relationships.rbの記述変更

--- db/migrate/20230314080404_add_index_to_report_reference_relationships.rb
+++ db/migrate/20230314080404_add_index_to_report_reference_relationships.rb
@@ -1,5 +1,5 @@
 class AddIndexToReportReferenceRelationships < ActiveRecord::Migration[7.0]
   def change
-    add_index :report_mention_relationships, [:mentioned_report_id, :mentioning_report_id], unique: true, name: 'report_mention_relationship_index'
+    add_index :report_reference_relationships, [:mentioned_report_id, :mentioning_report_id], unique: true, name: 'report_reference_relationships_index'
   end
 end
