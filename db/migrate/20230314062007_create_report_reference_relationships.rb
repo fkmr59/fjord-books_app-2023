65c7fd945a3dc3ab1e505b8c02662e687b0b307d
terumitt <f4k0m6r0@icloud.com>
2023-03-14 15:24:21 +0900

    db/migrate/20230314062007_create_report_reference_relationships.rbに記述を追加

--- db/migrate/20230314062007_create_report_reference_relationships.rb
+++ db/migrate/20230314062007_create_report_reference_relationships.rb
@@ -1,7 +1,9 @@
 class CreateReportReferenceRelationships < ActiveRecord::Migration[7.0]
   def change
     create_table :report_reference_relationships do |t|
-
+      t.integer :mentioning_report_id
+      t.integer :mentioned_report_id
+      
       t.timestamps
     end
   end
