0cbb6c7a341f22651df00cf164f1b0df40614de3
terumitt <f4k0m6r0@icloud.com>
2023-03-14 15:45:34 +0900

    Revert "app/models/report.rbに追記"
    This reverts commit 2f728f4cc2cad38bb015d63f45e089705f34d5cb.

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,14 +7,6 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
-  has_many :mentioned_from, class_name: 'ReportReferenceRelationship', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: 'mentioned_report'
-  has_many :mentioned_reports, through: :mentioned_from, source: :mentioning_report
-
-  has_many :mentioning_to, class_name: 'ReportReferenceRelationship', foreign_key: :mentioning_report_id, dependent: :destroy, inverse_of: 'mentioning_report'
-  has_many :mentioning_reports, through: :mentioning_to, source: :mentioned_report
-
-  after_save :create_mentioning_to
-
   def editable?(target_user)
     user == target_user
   end
