2d6d70d399b3c4347f0fdcac33b7e80429747dc2
terumitt <f4k0m6r0@icloud.com>
2023-03-14 16:01:29 +0900

    app/models/report_reference_relationship.rbの記述削除

--- app/models/report_reference_relationship.rb
+++ app/models/report_reference_relationship.rb
@@ -1,5 +1,3 @@
 class ReportReferenceRelationship < ApplicationRecord
-  belongs_to :mentioning_report, class_name: 'Report'
-  belongs_to :mentioned_report, class_name: 'Report'
-  validates :mentioned_report_id, uniqueness: { scope: :mentioning_report_id }
+  
 end
