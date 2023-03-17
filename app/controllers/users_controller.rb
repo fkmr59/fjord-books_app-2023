bca4f151be5b4bc79c27ad95a0287de73687ee0d
terumitt <f4k0m6r0@icloud.com>
2023-03-17 14:34:39 +0900

    app/controllers/users_controller.rbの記述変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: 'Add User'
+      redirect_to root_url
     else
       render :new
     end
