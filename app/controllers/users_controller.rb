45e1b6c19fda653efc157b9dca044bac956f3dbb
terumitt <f4k0m6r0@icloud.com>
2023-03-20 15:10:09 +0900

    app/controllers/users_controller.rbに追記とconfig/routes.rbの不要なコメント削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url
+      redirect_to root_url, notice: 'Add User'
     else
       render :new
     end
