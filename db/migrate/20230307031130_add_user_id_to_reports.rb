68457d84ce86b0c1d23e5400d2ad9dc8fd92a3f4
fkmr59 <f4k0m6r0@icloud.com>
2023-04-10 11:16:14 +0900

    DBを再構築

--- db/migrate/20230307031130_add_user_id_to_reports.rb
+++ db/migrate/20230307031130_add_user_id_to_reports.rb
@@ -1,5 +1,5 @@
 class AddUserIdToReports < ActiveRecord::Migration[7.0]
   def change
-    add_reference :reports, :user, foreign_key: true
+    add_reference :reports, :user, foreign_key: true, null: false
   end
 end
