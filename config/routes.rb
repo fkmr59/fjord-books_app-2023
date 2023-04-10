436a29c35d014d432ef1aa30dd74ef0c6ea095a8
fkmr59 <f4k0m6r0@icloud.com>
2023-04-10 10:27:25 +0900

    Flash機能が不要のため元に戻す

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # devise_for :users
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
